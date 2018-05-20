554c57dca10b70987fb78ab5b1cce5af0165b5c5
WiredSound <maxoblack@yahoo.com>
2018-05-20 12:26:35 +0100

    Some very minor adjustments.

--- src/GameState.cpp
+++ src/GameState.cpp
@@ -2,10 +2,10 @@
 
 GameState::GameState(sf::RenderWindow &renderWindow, sf::Font &textFont)
 	: State(renderWindow), turnManager(map), gui((sf::Vector2f) renderWindow.getSize()), mapBuilder(10, renderWindow, gui), font(textFont) {
-	map = mapBuilder.buildMap(sf::Vector2u(50, 50), sf::Vector2f(48, 48), sf::Vector2f(16, 16));
+	map = mapBuilder.buildMap(sf::Vector2u(50, 50), sf::Vector2f(64, 64), sf::Vector2f(16, 16));
 	map->construct();
 
-	gui.setup(turnManager, font, sf::Vector2f(1, 1), sf::Color(5, 5, 5, 210), sf::Color(5, 5, 5, 230), sf::Color(35, 140, 35, 255), 2);
+	gui.setup(turnManager, font, sf::Vector2f(1, 1), sf::Color(5, 5, 5, 200), sf::Color(5, 5, 5, 230), sf::Color(35, 140, 35, 255), 2);
 }
 
 void GameState::update(Input &input) {
